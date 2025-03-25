dc916273d27c60174c804dbd3dc26cd7636b140e
srushtiukani3116 <srushtiukani3116@gmail.com>
2025-03-25 14:14:36 +0530

    craete pages and route

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import React from 'react';
 import './index.css'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <App/>
+    </StrictMode>
+  </BrowserRouter>,
 )
